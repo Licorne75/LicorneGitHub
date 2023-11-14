8f2f1f1c371743fbb9aabf2047b08f8a48e238f2
SrouvFinaltis <srouviere@finaltis.com>
2023-11-14 15:45:11 +0100

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -8,11 +8,23 @@
             Console.WriteLine("Hello, C# Academy !, one more modification"); 
 
             myMethod();
+            Console.WriteLine(addition(3, 4));
+            Console.WriteLine(multiplication(3, 4));
         }
 
         static void myMethod()
         {
             Console.WriteLine("Now I have a method");
+        }
+
+        static double addition(double a, double b)
+        {
+            return a + b;
+        }
+
+        static double multiplication(double a, double b)
+        {
+            return a * b;
         }
     }
 }
