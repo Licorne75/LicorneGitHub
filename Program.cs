ed926192ead86b3dac2077225279ef1cea2f8949
Licorne75 <150789268+Licorne75@users.noreply.github.com>
2023-11-14 11:02:14 +0100

    Keeping one message

--- Program.cs
+++ Program.cs
@@ -5,9 +5,6 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, C# Academy !, one more modification"); Console.WriteLine("Hello, C# Academy !, one more modification"); Console.WriteLine("Hello, C# Academy !, one more modification"); Console.WriteLine("Hello, C# Academy !, one more modification");
-            Console.WriteLine("Hello, C# Academy !, one more modification"); Console.WriteLine("Hello, C# Academy !, one more modification");
-            Console.WriteLine("Hello, C# Academy !, one more modification");
         }
     }
 }
