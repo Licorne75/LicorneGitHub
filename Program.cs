14d2b71d4f59c0ec6420c8663d6e3272a9fd1907
Licorne75 <150789268+Licorne75@users.noreply.github.com>
2023-11-14 10:45:34 +0100

    one more modification

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, C# Academy !");
+            Console.WriteLine("Hello, C# Academy !, one more modification");
         }
     }
 }
